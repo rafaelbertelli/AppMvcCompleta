2438baf4da1e1a7a2f40a5e98d6383bf8e8cc111
Rafael Borges <rafaelbertelli89@gmail.com>
2020-03-14 10:55:30 -0300

    matches apps versions to install EF

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
