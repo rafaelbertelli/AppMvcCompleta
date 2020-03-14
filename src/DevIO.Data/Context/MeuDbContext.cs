a3e908f68254b41eac4974213d6a9bed52cbddfd
Rafael Borges <rafaelbertelli89@gmail.com>
2020-03-14 11:12:46 -0300

    creates references from business model to initialize db tables

--- src/DevIO.Data/Context/MeuDbContext.cs
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DevIO.Business.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace DevIO.Data.Context
@@ -8,8 +9,12 @@ namespace DevIO.Data.Context
     public class MeuDbContext : DbContext
     {
         public MeuDbContext(DbContextOptions options) : base(options)
-        {
+        { }
 
-        }
+        public DbSet<Produto> Produtos { get; set; }
+
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
     }
 }
