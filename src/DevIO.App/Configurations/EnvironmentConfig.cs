73f949fec6142e1ce4802bcf4a5c6b5fc38a94d6
Rafael Borges <rafael.borges@luizalabs.com>
2020-05-15 22:29:16 -0300

    add error routes

--- src/DevIO.App/Configurations/EnvironmentConfig.cs
+++ src/DevIO.App/Configurations/EnvironmentConfig.cs
@@ -14,7 +14,8 @@ namespace DevIO.App.Configurations
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
